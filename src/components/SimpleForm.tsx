a7765ed88e1118316128c5840f865f070c4d04d0
mike.mo <>
2022-06-24 19:05:36 +1000

    add state for the form field

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SimpleForm = () => {
+    const [userName, setUserName] = useState<string>("")    // 2. type of the state object, 3. don't forget the const!
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>)  => { // 1. event type
         event.preventDefault()
-        alert('Form submitted')
+        alert('Form submitted ' + userName)
     }
     return <div>
         <div>Simple Form</div>
         <form onSubmit={handleSubmit}>
             <fieldset>
                 <label>Name</label>
-                <input name="name"></input>
+                <input name="userName" onChange={e => setUserName(e.target.value)} ></input>
             </fieldset>
             <button type="submit">Submit</button>
         </form>
