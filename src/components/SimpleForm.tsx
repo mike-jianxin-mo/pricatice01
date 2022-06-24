caee8207b2b38931a674a1c4ee507101556d697f
mike.mo <>
2022-06-24 18:06:43 +1000

    add form template

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const SimpleForm = () => {
-    return <div>Simple Form</div>
+    return <div>
+        <div>Simple Form</div>
+        <form>
+            <fieldset>
+                <label>Name</label>
+                <input name="name"></input>
+            </fieldset>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 }
 
 export default SimpleForm
