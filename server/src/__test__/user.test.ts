0478a68f7e03018da1afeadcd91b3ecde7d87a09
mike.mo <>
2022-06-25 21:12:51 +1000

    add complex test function, but fail due to the missing clear function for the server data

--- server/src/__test__/user.test.ts
+++ server/src/__test__/user.test.ts
@@ -8,5 +8,19 @@ describe('test simple user functions', () => {
             .send({userName: 'mike mo'})
             .expect(201)
     })
+
+    it('add user', async () => {
+        await request(app)
+            .post('/users')
+            .send({userName: 'Jeff'})
+            .expect(201)
+
+        const response = await request(app)
+            .get('/users')
+        
+        // affected by the previous test, the user name is 'mike mo'
+        // data should be re-init before run it.
+        expect(response.body[0].userName).toEqual('Jeff')
+    })
 })
 
