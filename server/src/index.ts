9249197df74d26a05818ce9407b4aa2826e4fb4e
mike.mo <>
2022-06-25 12:21:51 +1000

    add simple user functions(add/list)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,33 @@
 import express, {Request, Response} from 'express'
+import {randomBytes} from 'crypto'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 9000
 
+app.use(bodyParser.json())
+
+type User = {
+    id: string;
+    userName: string;
+}
+
+const users: User[] = []
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World')
+})
+
+app.get('/users', (req: Request, res: Response) => {
+    res.send(users)
+})
+
+app.post('/users', (req: Request, res: Response) => {
+    const id = randomBytes(4).toString("hex")
+    const {userName} = req.body
+    const user = {id, userName}
+    users.push(user)
+    res.status(201).send(user)
 })
 
 app.listen(port, () => {
